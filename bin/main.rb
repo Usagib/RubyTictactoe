da9a7bd7d1579b2a484624770696897abdeffcb5
nyan lin tun <devkonyan@gmail.com>
2019-12-13 03:50:32 +0900

    fix adding looping

--- bin/main.rb
+++ bin/main.rb
@@ -19,8 +19,18 @@ puts "Enter your second player name?"
 secondPlayer = gets.chomp
 secondChar = findSecondChar(firstChar)
 puts "#{secondPlayer} is auto choosing #{secondChar}"
-puts "#{firstPlayer} turn"
+print "###\n ###\n ###\n"
+puts "Game is starting ....."
 puts "#{['1', '2', '3']}"
 puts "#{['4', '5', '6']}"
 puts "#{['7', '8', '9']}"
+
+#while 1..9 time looping ....
+puts "#{firstPlayer} turn , choose a cell[1-9]:"
+# get input from firstPlayer
+# show the pattern 
+# compare the winning pattern
+# update 
+#loop end here
+
 puts "Winner: #{secondPlayer}"
