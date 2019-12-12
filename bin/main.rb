fedaa052ed498fa1a450295a18cbf5824236dcbc
nyan lin tun <devkonyan@gmail.com>
2019-12-13 02:40:43 +0900

    add and choosing player

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,23 @@
 #!/usr/bin/env ruby
 
+def findSecondChar(val)
+    if val == "X"
+        return "O"
+    else
+        return "X"
+    end
+end 
+
 puts "Welcome from the world Tic Tac Toe"
+puts "Enter your first player name?"
+firstPlayer = gets.chomp
+puts "Welcome #{firstPlayer}, Please Choose your (X / 0):"
+firstChar = gets.chomp
+puts "#{firstPlayer} is choosing #{firstChar}"
+puts "####################"
+puts "Enter your second player name?"
+secondPlayer = gets.chomp
+secondChar = findSecondChar(firstChar)
+puts "#{secondPlayer} is auto choosing #{secondChar}"
+
+
