b440446ed29798120b8e9bdd51f54980f40fa33c
Eduardo Bonilla <eduardobonillac@gmail.com>
2019-12-12 12:00:58 -0600

    board and winner flow added

--- bin/main.rb
+++ bin/main.rb
@@ -6,7 +6,7 @@ def findSecondChar(val)
     else
         return "X"
     end
-end 
+end
 
 puts "Welcome from the world Tic Tac Toe"
 puts "Enter your first player name?"
@@ -19,5 +19,8 @@ puts "Enter your second player name?"
 secondPlayer = gets.chomp
 secondChar = findSecondChar(firstChar)
 puts "#{secondPlayer} is auto choosing #{secondChar}"
-
-
+puts "#{firstPlayer} turn"
+puts "#{['1', '2', '3']}"
+puts "#{['4', '5', '6']}"
+puts "#{['7', '8', '9']}"
+puts "Winner: #{secondPlayer}"
