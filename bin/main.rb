2e165718d578338f990f7d90917e8aa12803a436
nyan lin tun <devkonyan@gmail.com>
2019-12-17 00:43:43 +0900

    separate class

--- bin/main.rb
+++ bin/main.rb
@@ -1,112 +1,7 @@
 #!/usr/bin/env ruby
-class Tictactoe
-  def initialize()
-    @player1 = Player.new(1, 'X')
-    @player2 = Player.new(2, 'O')
-    @board = Board.new
-    start()
-  end
-end
 
-class Player
-  def initialize(name = nil, symbol = nil)
-      @name = name
-      @symbol = symbol
-  end
-
-  def set_name(name)
-    @name = name
-  end
-
-  def set_symbol(symbol)
-    @symbol = symbol
-  end
-
-  def get_name
-    @name
-  end
-
-  def get_symbol
-    @symbol
-  end
-
-end
-
-class Board
-    def initialize
-        @@board = [
-                  ["1", "2", "3"],
-                  ["4", "5", "6"],
-                  ["7", "8", "9"]
-                ]
-    end
-
-    def move(cell, player)
-      num = cell.to_i
-      if num == 1
-        @@board[0][0] != "1" ? false : @@board[0][0] = player.get_symbol
-      end
-      if num == 2
-        @@board[0][1] != "2" ? false : @@board[0][1] = player.get_symbol
-      end
-      if num == 3
-        @@board[0][2] != "3" ? false : @@board[0][2] = player.get_symbol
-      end
-      if num == 4
-        @@board[1][0] != "4" ? false : @@board[1][0] = player.get_symbol
-      end
-      if num == 5
-        @@board[1][1] != "5" ? false : @@board[1][1] = player.get_symbol
-      end
-      if num == 6
-        @@board[1][2] != "6" ? false : @@board[1][2] = player.get_symbol
-      end
-      if num == 7
-        @@board[2][0] != "7" ? false : @@board[2][0] = player.get_symbol
-      end
-      if num == 8
-        @@board[2][1] != "8" ? false : @@board[2][1] = player.get_symbol
-      end
-      if num == 9
-        @@board[2][2] != "9" ? false : @@board[2][2] = player.get_symbol
-      end
-    end
-
-    def print_board
-      puts "#{@@board[0]}"
-      puts "#{@@board[1]}"
-      puts "#{@@board[2]}"
-    end
-
-    def wincondition
-      if @@board[0][0] == @@board[0][1] && @@board[0][0] == @@board[0][2] && @@board[0][1] == @@board[0][2]
-        return true
-      end
-      if @@board[1][0] == @@board[1][1] && @@board[1][0] == @@board[1][2] && @@board[1][1] == @@board[1][2]
-        return true
-      end
-      if @@board[2][0] == @@board[2][1] && @@board[2][0] == @@board[2][2] && @@board[2][1] == @@board[2][2]
-        return true
-      end
-      if @@board[0][0] == @@board[1][0] && @@board[0][0] == @@board[2][0] && @@board[1][0] == @@board[2][0]
-        return true
-      end
-      if @@board[0][1] == @@board[1][1] && @@board[0][1] == @@board[2][1] && @@board[1][1] == @@board[2][1]
-        return true
-      end
-      if @@board[0][2] == @@board[1][2] && @@board[0][2] == @@board[2][2] && @@board[1][2] == @@board[2][2]
-        return true
-      end
-      if @@board[0][0] == @@board[1][1] && @@board[0][0] == @@board[2][2] && @@board[1][1] == @@board[2][2]
-        return true
-      end
-      if @@board[0][2] == @@board[1][1] && @@board[0][2] == @@board[2][0] && @@board[1][1] == @@board[2][0]
-        return true
-      end
-      false
-    end
-end
-
+require_relative "../lib/board.rb"
+require_relative "../lib/player.rb"
 
 def findSecondChar(val)
     if val == "X"
@@ -115,6 +10,8 @@ def findSecondChar(val)
         return "X"
     end
 end
+
+
 puts "Welcome from the world Tic Tac Toe"
 puts "Enter your first player name?"
 firstPlayer = Player.new
