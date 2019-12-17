40ee4f9ff663d358bc0fd80550fb4d4af50a8b9d
nyan lin tun <devkonyan@gmail.com>
2019-12-18 00:37:03 +0900

    add and fix game layout and player and game_logic

--- bin/main.rb
+++ bin/main.rb
@@ -3,50 +3,72 @@
 require_relative "../lib/board.rb"
 require_relative "../lib/player.rb"
 
-def findSecondChar(val)
-    if val == "X"
-        return "O"
-    else
-        return "X"
-    end
-end
+# puts "Welcome from the world Tic Tac Toe"
+# puts "Enter your first player name?"
+# firstPlayerName = gets.chomp
+# firstPlayer = firstPlayerName.empty? ? Player.new("firstPlayer", "X") :
+#   Player.new(firstPlayerName, "X")
+# puts "Welcome #{firstPlayer.name}, Your Radomly choose #{firstPlayer.sign}"
+# puts "#################################"
 
+# puts "Enter your second player name?"
+# secondPlayerName = gets.chomp
+# secondPlayer = secondPlayerName.empty? ? Player.new("secondPlayer", "O") :
+#   Player.new(secondPlayerName, "O")
 
-puts "Welcome from the world Tic Tac Toe"
-puts "Enter your first player name?"
-firstPlayer = Player.new
-firstPlayer.set_name(gets.chomp)
-puts "Welcome #{firstPlayer.get_name}, Please Choose your (X / 0):"
-firstPlayer.set_symbol(gets.chomp)
-puts "#{firstPlayer.get_name} is choosing #{firstPlayer.get_symbol}"
-puts "####################"
-puts "Enter your second player name?"
-secondPlayer = Player.new
-secondPlayer.set_name(gets.chomp)
-secondPlayer.set_symbol(findSecondChar(firstPlayer.get_symbol))
-puts "#{secondPlayer.get_name} is auto choosing #{secondPlayer.get_symbol}"
-print "###\n ###\n ###\n"
+# puts "Welcome #{secondPlayer.name}, Your choose is #{secondPlayer.sign}"
+
+# puts "\t### \t### \t### \n\t### \t### \t### \n\t### \t### \t###"
+# puts "###################################"
+# puts "###################################"
+# puts "\t### \t### \t### \n\t### \t### \t### \n\t### \t### \t###"
+# puts "###################################"
+# puts "###################################"
+# puts "\t### \t### \t### \n\t### \t### \t### \n\t### \t### \t###"
+
 puts "Game is starting ....."
 gameBoard = Board.new
-gameBoard.print_board
+
+puts gameBoard.print_board
+
 game_on = true
 i = 0
 winner = true
-while game_on
-  i = i + 1
-  player = i % 2  == 0 ? firstPlayer : secondPlayer;
-  puts "#{player.get_name} turn , choose a cell[1-9]:"
-  gameBoard.move(gets.chomp, player)
-  gameBoard.print_board
-  if gameBoard.wincondition == true
-     game_on = false
-   else
-     puts "keep playing ..."
-   end
 
-  if i >= 9 #winner pattern match
+firstPlayer = Player.new("nyan", "O")
+# firstPlayer.add_moves(1)
+# firstPlayer.add_moves(2)
+# firstPlayer.add_moves(3)
+secondPlayer = Player.new("ko", "X")
+# secondPlayer.add_moves(4)
+# secondPlayer.add_moves(5)
+# secondPlayer.add_moves(6)
+
+while game_on && i < 10
+  i += 1
+  player = i % 2 == 0 ? firstPlayer : secondPlayer
+  puts "#{player.name} turn , choose a cell[1-9]:"
+
+  move = gets.chomp.to_i
+
+  until gameBoard.board_cells.include?(move)
+    puts "Your move is duplicated, Please input a number between 1-9 in the available moves on the board"
+    move = gets.chomp.to_i
+  end
+
+  player.add_moves(move)
+  gameBoard.board_cells[move - 1] = player.sign
+  gameBoard.check_winning_move(player)
+
+  puts gameBoard.print_board
+
+  if player.winner
+    puts "The winner is: #{player.name}!"
     game_on = false
+    break
+  end
+  if i == 9
+    puts "The game is tie."
+    break;
   end
 end
-
-puts "Winner: #{player.get_name}"
