ef42c981634d06146cb61a151ce249ed8f87df7b
nyan lin tun <devkonyan@gmail.com>
2019-12-18 01:29:50 +0900

    fix ruboco and ruby guide line

--- bin/main.rb
+++ bin/main.rb
@@ -5,18 +5,18 @@ require_relative "../lib/player.rb"
 
 puts "Welcome from the world Tic Tac Toe"
 puts "Enter your first player name?"
-firstPlayerName = gets.chomp
-firstPlayer = firstPlayerName.empty? ? Player.new("firstPlayer", "X") :
-  Player.new(firstPlayerName, "X")
-puts "Welcome #{firstPlayer.name}, Your Radomly choose #{firstPlayer.sign}"
+first_player_name = gets.chomp
+first_player = first_player_name.empty? ? Player.new("firstPlayer", "X") :
+  Player.new(first_player_name, "X")
+puts "Welcome #{first_player.name}, Your Radomly choose #{first_player.sign}"
 puts "#################################"
 
 puts "Enter your second player name?"
-secondPlayerName = gets.chomp
-secondPlayer = secondPlayerName.empty? ? Player.new("secondPlayer", "O") :
-  Player.new(secondPlayerName, "O")
+second_player_name = gets.chomp
+second_player = second_player_name.empty? ? Player.new("second_player", "O") :
+  Player.new(second_player_name, "O")
 
-puts "Welcome #{secondPlayer.name}, Your choose is #{secondPlayer.sign}"
+puts "Welcome #{second_player.name}, Your choose is #{second_player.sign}"
 
 puts "\t### \t### \t### \n\t### \t### \t### \n\t### \t### \t###"
 puts "###################################"
@@ -27,28 +27,28 @@ puts "###################################"
 puts "\t### \t### \t### \n\t### \t### \t### \n\t### \t### \t###"
 
 puts "Game is starting ....."
-gameBoard = Board.new
-puts gameBoard.print_board
+game_board = Board.new
+puts game_board.print_board
 game_on = true
 i = 0
 while game_on && i < 10
   i += 1
-  player = i % 2 == 0 ? firstPlayer : secondPlayer
+  player = (i % 2).zero? ? first_player : second_player
   puts "#{player.name} turn , choose a cell[1-9]:"
 
   move = gets.chomp.to_i
   puts "#{player.name},you choose #{move} :"
 
-  until gameBoard.board_cells.include?(move)
+  until game_board.board_cells.include?(move)
     puts "Your move is duplicated, Please input a number between 1-9 in the available moves on the board"
     move = gets.chomp.to_i
   end
 
   player.add_moves(move)
-  gameBoard.board_cells[move - 1] = player.sign
-  gameBoard.check_winning_move(player)
+  game_board.board_cells[move - 1] = player.sign
+  game_board.check_winning_move(player)
 
-  puts gameBoard.print_board
+  puts game_board.print_board
 
   if player.winner
     puts "The winner is: #{player.name}!"
