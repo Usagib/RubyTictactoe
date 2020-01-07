59cd211739c8052fb7a3c85d845660ffa95b0ce2
nyan lin tun <devkonyan@gmail.com>
2020-01-08 08:40:19 +0900

    add player testing

---
+++ spec/player_spec.rb
@@ -0,0 +1,19 @@
+require "player"
+
+RSpec.describe Player do
+  let(:player) { Player.new("Nyan", "X") }
+
+  it "has player's name" do
+    expect(player.name).to eq("Nyan")
+  end
+
+  it "has player's sign" do
+    expect(player.sign).to eq("X")
+  end
+
+  it "can move and save his moves" do
+    player.add_moves(2)
+    player.add_moves(3)
+    expect(player.moves).to eq([2, 3])
+  end
+end
